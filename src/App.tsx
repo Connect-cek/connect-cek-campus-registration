460bb3c1610708a3dd77846b2875ca19686ce2db
Abhimanyu R B <abhimanyurbsa@gmail.com>
2025-02-28 23:16:13 +0530

    fix(hero): removed unwanted sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import ValueProposition from './components/ValueProposition';
-import WhyConnect from './components/WhyConnect';
 import MissionVision from './components/MissionVision';
 import TestimonialSection from './components/TestimonialSection';
 import Footer from './components/Footer';
@@ -14,10 +12,6 @@ const App: React.FC = () => {
       <main>
         <Hero />
         <div className="content-container">
-          <div className="flex flex-col md:flex-row gap-6 px-4 md:px-8 max-w-7xl mx-auto -mt-16">
-            <ValueProposition />
-            <WhyConnect />
-          </div>
           <div className="px-4 md:px-8 max-w-7xl mx-auto mt-6">
             <MissionVision />
           </div>
